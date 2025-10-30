d3ed2dcdbc6e8df5ce51859c198e300cc74e10c5
KARLI Gyula Sámuel <karli.samu@gmail.com>
2025-10-30 09:36:39 +0100

    EoD

--- Src/Geometry/SubPolygon.cpp
+++ Src/Geometry/SubPolygon.cpp
@@ -5,66 +5,66 @@
 using namespace Geometry;
 
 namespace S {
-    SubPolygon::SubPolygon(Array<API_Coord>* coords, Array<API_PolyArc>* pars, Array<UInt32>* vertexIDs, bool i_isHole)
-		: m_isHole(i_isHole)
+  SubPolygon::SubPolygon(Array<API_Coord>* coords, Array<API_PolyArc>* pars, Array<UInt32>* vertexIDs, bool i_isHole)
+    : m_isHole(i_isHole)
+  {
+    Coord* centerPrev = NULL;
+    Coord pointPrev = (*coords)[0], pointThis;
+
+    Coord centerThis;
+    UINT firstIdx = 0;
+    double eps = 0, radEps = 0;
+
+    const Segment s((*coords)[0], (*coords)[1]);
+    Sector midPerpPrev = s.MidPerp().ToSector();
+
+    for (UInt16 i = 1; i < coords->GetSize(); ++i)
     {
-        Coord *centerPrev = NULL;
-        Coord pointPrev = (*coords)[0], pointThis;
+      pointThis = (*coords)[i];
+      Segment s(pointPrev, pointThis);
 
-        Coord centerThis;
-        UINT firstIdx = 0;
-        double eps = 0, radEps = 0;
+      const Sector midPerpThis = s.MidPerp().ToSector();
 
-        const Segment s((*coords)[0], (*coords)[1]);
-        Sector midPerpPrev = s.MidPerp().ToSector();
+      XLinesEps(midPerpPrev, midPerpThis, &centerThis, eps, radEps);
 
-        for(UInt16 i = 1; i < coords->GetSize(); ++i)
+      if (centerPrev != NULL && centerThis != *centerPrev)
+      {
+        if (i > firstIdx + 1)
         {
-            pointThis = (*coords)[i];
-            Segment s(pointPrev, pointThis);
+          Segment newSeg(pointPrev, pointThis);
 
-            const Sector midPerpThis = s.MidPerp().ToSector();
-
-            XLinesEps(midPerpPrev, midPerpThis, &centerThis, eps, radEps);
-
-            if (centerPrev != NULL && centerThis != *centerPrev)
-            {
-                if (i > firstIdx + 1)
-                {
-                    Segment newSeg(pointPrev, pointThis);
-
-                    //(*segments)[firstIdx]->SetArc(0, centerPrev);
-                    //TODO
-                }
-
-                firstIdx = i;
-            }
-
-            *centerPrev = centerThis;
-            midPerpPrev = midPerpThis;
-        }
-    }
-
-    SubPolygon::SubPolygon(const SubPolygon& i_other)
-    {
-		Segment* _prevSeg = nullptr;
-
-        m_segments.SetCapacity(i_other.m_segments.GetSize());
-        for (Segment* s : i_other.m_segments)
-        {
-			Segment* newSeg = new Segment(*s);
-            m_segments.Push(newSeg);
-            if (_prevSeg != nullptr)
-            {
-                newSeg->SetPrev(_prevSeg);
-                _prevSeg->SetNext(newSeg);
-			}
-            _prevSeg = newSeg;
+          //(*segments)[firstIdx]->SetArc(0, centerPrev);
+          //TODO
         }
 
-		m_segments[0]->SetPrev(m_segments.GetLast());
-		m_segments.GetLast()->SetNext(m_segments[0]);
+        firstIdx = i;
+      }
+
+      *centerPrev = centerThis;
+      midPerpPrev = midPerpThis;
     }
+  }
+
+  SubPolygon::SubPolygon(const SubPolygon& i_other)
+  {
+    Segment* _prevSeg = nullptr;
+
+    m_segments.SetCapacity(i_other.m_segments.GetSize());
+    for (Segment* s : i_other.m_segments)
+    {
+      Segment* newSeg = new Segment(*s);
+      m_segments.Push(newSeg);
+      if (_prevSeg != nullptr)
+      {
+        newSeg->SetPrev(_prevSeg);
+        _prevSeg->SetNext(newSeg);
+      }
+      _prevSeg = newSeg;
+    }
+
+    m_segments[0]->SetPrev(m_segments.GetLast());
+    m_segments.GetLast()->SetNext(m_segments[0]);
+  }
 
   //  SubPolygon::SubPolygon(SubPolygon&& i_other) noexcept
   //  {
@@ -73,178 +73,196 @@ namespace S {
   //      {
   //          m_segments.Push(s);
   //      }
-		//i_other.m_segments.Clear();
+    //i_other.m_segments.Clear();
   //  }
 
-    Segment* SubPolygon::getShortestSegment() const
-    { 
-        Segment* _shortestSeg = nullptr;
+  Segment* SubPolygon::GetShortestSegment() const
+  {
+    Segment* _shortestSeg = nullptr;
 
-        for (auto s : m_segments)
+    for (auto s : m_segments)
+    {
+      if (_shortestSeg == nullptr || s->GetLength() < _shortestSeg->GetLength())
+        _shortestSeg = s;
+    }
+
+    return _shortestSeg;
+  }
+
+  //SubPolygon& SubPolygon::operator=(const SubPolygon& other) {
+  //    if (this != &other) {
+  //        SubPolygon temp(other);
+  //        using std::swap;
+  //        swap((*this).m_segments, temp.m_segments);
+  //    }
+  //    return *this;
+  //}
+
+  void SubPolygon::RemoveShortestSegment()
+  {
+    Segment* shortestSegment = GetShortestSegment();
+    Segment* _prevSeg = shortestSegment->GetPrev();
+    Segment* _nextSeg = shortestSegment->GetNext();
+
+    logger.Log(GS::UniString("Removing segment: ") + shortestSegment->ToString(LogFormat::Short));
+
+    _prevSeg->Intersect(_nextSeg);
+
+    RemoveSegment(shortestSegment);
+  }
+
+  double SubPolygon::GetShortestEdgeLength() const
+  {
+    double _minLength;
+
+    if (m_segments.GetSize() == 0)
+      return 0.0;
+    else
+      _minLength = DBL_MAX;
+
+    for (auto s : m_segments)
+    {
+      double l = s->GetLength();
+
+      if (s->GetLength() < _minLength)
+        _minLength = l;
+    }
+
+    return _minLength;
+  }
+
+  std::string SubPolygon::ToString() const
+  {
+    std::string result = "";
+
+    for (auto s : m_segments)
+    {
+      result += s->ToString(LogFormat::Default) + std::string(" ");
+    }
+
+    // Remove last space
+    if (!result.empty())
+      result.pop_back();
+
+    return result;
+  }
+
+  // --
+
+  void SubPolygon::Preprocess()
+  {
+    RemoveCollinear();
+    PolyToArc();
+  }
+
+  void SubPolygon::Postprocess()
+  {
+    //ArcToPoly();
+  }
+
+  // ----
+
+  //void _createArc(Array<Segment*> &io_arc, const Coord* i_prevMidPerpIntSectPt, Array <Segment*> &io_delArc)
+  //{
+  //    UINT minSize = 3;
+
+  //    if (io_arc.GetSize() > minSize)
+  //    {
+  //        io_arc[0]->SetCenter(*i_prevMidPerpIntSectPt);
+  //        io_arc[0]->SetEnd(*io_arc.GetLast()->GetEnd());
+
+  //        for (Segment* sToDelete : io_arc.Slice(1, io_arc.GetSize()))
+  //        {
+  //          io_delArc.Push(sToDelete);
+  //        }
+  //    }
+
+  //    io_arc.Clear();
+  //}
+
+  void SubPolygon::PolyToArc()
+  {
+    Segment* prevSegment = m_segments[-1];
+    std::unique_ptr<Coord> prevMidPerpIntSectPt = std::make_unique<Coord>(), midPerpIntSectPt = std::make_unique<Coord>() ;
+    Array<Segment*> arc, _delarc;
+    UINT minSize = 3;
+
+    for (Segment* s : m_segments)
+    {
+      std::optional<Coord> _int = s->IntersectMidPerp(prevSegment);
+
+      if (_int)
+        *midPerpIntSectPt = *_int;
+      else
+        midPerpIntSectPt.reset();
+
+      if (prevMidPerpIntSectPt && midPerpIntSectPt)
+        if (*prevMidPerpIntSectPt == *midPerpIntSectPt)
         {
-            if (_shortestSeg == nullptr || s->GetLength() < _shortestSeg->GetLength())
-                _shortestSeg = s;
-		}
+          if (arc.GetSize() == 0)
+          {
+            std::optional<int> _index = m_segments.IndexOf(s);
+            if (_index)
+            {
+              arc.Push(m_segments[*_index - 2]);
+              arc.Push(m_segments[*_index - 1]);
+            }
+          }
 
-        return _shortestSeg;
-    }
-
-    //SubPolygon& SubPolygon::operator=(const SubPolygon& other) {
-    //    if (this != &other) {
-    //        SubPolygon temp(other);
-    //        using std::swap;
-    //        swap((*this).m_segments, temp.m_segments);
-    //    }
-    //    return *this;
-    //}
-
-    void SubPolygon::removeShortestSegment()
-    {
-        Segment* shortestSegment = getShortestSegment();
-        Segment* _prevSeg = shortestSegment->GetPrev();
-        Segment* _nextSeg = shortestSegment->GetNext();
-
-        logger.Log(GS::UniString("Removing segment: ") + shortestSegment->ToString(LogFormat::Short));
-
-        _prevSeg->Intersect(_nextSeg);
-
-        removeSegment(shortestSegment);
-    }
-
-    double SubPolygon::GetShortestEdgeLength() const
-    {
-        double _minLength;
-
-        if (m_segments.GetSize() == 0)
-			return 0.0;
+          arc.Push(s);
+        }
         else
-			_minLength = DBL_MAX;
-
-        for (auto s : m_segments)
         {
-			double l = s->GetLength();
+          //_createArc(arc, prevMidPerpIntSectPt, _delarc);
 
-			if (s->GetLength() < _minLength)
-                _minLength = l;
-        }
+          UINT minSize = 3;
 
-		return _minLength;
-    }
+          if (arc.GetSize() > minSize)
+          {
+            arc[0]->SetCenter(*prevMidPerpIntSectPt);
+            arc[0]->SetEnd(*arc.GetLast()->GetEnd());
 
-    std::string SubPolygon::ToString() const
-    {
-        std::string result = "";
-
-        for (auto s : m_segments)
-        {
-            result += s->ToString(LogFormat::Default) + std::string(" ");
-        }
-
-        // Remove last space
-        if (!result.empty())
-            result.pop_back();
-
-        return result;
-    }
-
-    // --
-
-    void SubPolygon::Preprocess()
-    {
-        RemoveCollinear();
-        PolyToArc();
-    }
-
-    void SubPolygon::Postprocess()
-    {
-        //ArcToPoly();
-    }
-
-    // ----
-
-    void _createArc(Array<Segment*> &io_arc, const Coord* i_prevMidPerpIntSectPt, CircularArray <Segment*> &io_segments)
-    {
-        UINT minSize = 3;
-
-        if (io_arc.GetSize() > minSize)
-        {
-            io_arc[0]->SetCenter(*i_prevMidPerpIntSectPt);
-            io_arc[0]->SetEnd(*io_arc.GetLast()->GetEnd());
-
-            for (Segment* sToDelete : io_arc.Slice(1, io_arc.GetSize()))
+            for (Segment* sToDelete : arc.Slice(1, arc.GetSize()))
             {
-                io_segments.DeleteAll(sToDelete);
+              _delarc.Push(sToDelete);
             }
+          }
+
+          arc.Clear();
         }
 
-        io_arc.Clear();
+      if (midPerpIntSectPt)
+        *prevMidPerpIntSectPt = *midPerpIntSectPt;
+
+      prevSegment = s;
     }
 
-    void SubPolygon::PolyToArc()
+    // FIXME if arc goes beyond the end and continues at the beginning
+
+    delete prevSegment;
+
+    for (Segment* sToDelete : _delarc)
     {
-
-        Segment* prevSegment = m_segments[-1];
-        Coord* prevMidPerpIntSectPt = new Coord, * midPerpIntSectPt = new Coord;
-        Array<Segment*> arc;
-        UINT minSize = 3;
-
-        for (Segment* s : m_segments)
-        {
-            std::optional<Coord> _int = s->IntersectMidPerp(prevSegment);
-
-            if (_int)
-                midPerpIntSectPt = &(*_int);
-            else
-				midPerpIntSectPt = nullptr;
-
-            if (prevMidPerpIntSectPt && midPerpIntSectPt)
-                if (*prevMidPerpIntSectPt == *midPerpIntSectPt)
-                {
-                    if (arc.GetSize() == 0)
-                    {
-                        std::optional<int> _index = m_segments.IndexOf(s);
-                        if (_index)
-                        {
-                            arc.Push(m_segments[*_index - 2]);
-                            arc.Push(m_segments[*_index - 1]);
-                        }
-                    }
-                    arc.Push(s);
-                }
-                else
-                {
-                    _createArc(arc, prevMidPerpIntSectPt, m_segments);
-                }
-
-            if (midPerpIntSectPt)
-                *prevMidPerpIntSectPt = *midPerpIntSectPt;
-
-            prevSegment = s;
-        }
-
-        if (arc.GetSize() )
-			// FIXME if arc goes beyond the end and continues at the beginning
-            _createArc(arc, prevMidPerpIntSectPt, m_segments);
-
-        delete prevSegment, prevMidPerpIntSectPt, midPerpIntSectPt;
+      m_segments.DeleteAll(sToDelete);
     }
+  }
 
-    void SubPolygon::RemoveCollinear()
+  void SubPolygon::RemoveCollinear()
+  {
+    Segment* prevSegment = m_segments[-1];
+
+    for (Segment* s : m_segments)
     {
-        Segment* prevSegment = m_segments[-1];
+      if (s->IsCollinear(prevSegment))
+      {
+        prevSegment->SetEnd(*s->GetEnd());
+        m_segments.DeleteAll(s);
+      }
 
-        for (Segment *s: m_segments)
-        {
-            if (s->IsCollinear(prevSegment) )
-            {
-                prevSegment->SetEnd(*s->GetEnd());
-                m_segments.DeleteAll(s);
-            }
-
-            prevSegment = s;
-        }
+      prevSegment = s;
     }
+  }
 
-    void SubPolygon::ArcToPoly(){}
+  void SubPolygon::ArcToPoly() {}
 }
 
--- Src/Gui/InfoboxPanel.hpp
+++ Src/Gui/InfoboxPanel.hpp
@@ -14,79 +14,79 @@
 extern Logger logger;
 
 namespace PolygonReducer {
-	S::Polygon* GetFirstPolygonFromSelection(API_Guid* o_guid = NULL);
+  S::Polygon* GetFirstPolygonFromSelection(API_Guid* o_guid = NULL);
 
-	// --- PolygonReducerPage ---------------------------------------------------------
+  // --- PolygonReducerPage ---------------------------------------------------------
 
-	class PolygonReducerInfoboxPage : 
-		public DG::TabPage
-	{
-		friend class	PolygonReducerPageObserver;
+  class PolygonReducerInfoboxPage :
+    public DG::TabPage
+  {
+    friend class	PolygonReducerPageObserver;
 
-	protected:
-		enum Controls  {
-			iUIPointNumberId = 2,
-			iUISliderId = 3,
-			SettingsButtonId = 4,
-			GDLButtonId = 5,
-			iUISmallestLengthId,a
-		};
-	
-		DG::PosIntEdit			iUIPointNumber;
-		DG::Slider				iSlider;
-		DG::Button				GDLButton;
-		DG::Button				SettingsButton;
-		TBUI::IAPIToolUIData*	uiData;
-		S::Polygon*				m_currentPolygon;
+  protected:
+    enum Controls {
+      iUIPointNumberId = 2,
+      iUISliderId = 3,
+      SettingsButtonId = 4,
+      GDLButtonId = 5,
+      iUISmallestLengthId, a
+    };
 
-	public:
-		PolygonReducerInfoboxPage(const DG::TabControl& tabControl, TBUI::IAPIToolUIData* puiData);
-		~PolygonReducerInfoboxPage();
-		void SetCurrentPolygon(S::Polygon* currentPolygon);
-		int GetPointNumber();
-		GSErrCode SetPointNumber(int/* p_val, int p_max*/);
-	};
+    DG::PosIntEdit			iUIPointNumber;
+    DG::Slider				iSlider;
+    DG::Button				GDLButton;
+    DG::Button				SettingsButton;
+    TBUI::IAPIToolUIData* uiData;
+    S::Polygon* m_currentPolygon;
+
+  public:
+    PolygonReducerInfoboxPage(const DG::TabControl& tabControl, TBUI::IAPIToolUIData* puiData);
+    ~PolygonReducerInfoboxPage();
+    void SetCurrentPolygon(S::Polygon* currentPolygon);
+    int GetPointNumber();
+    GSErrCode SetPointNumber(int/* p_val, int p_max*/);
+  };
 
 
-	// --- PolygonReducerPageObserver -------------------------------------------------
+  // --- PolygonReducerPageObserver -------------------------------------------------
 
-	class PolygonReducerPageObserver : 
-		public	DG::TextEditBaseObserver,
-		public	DG::PosIntEditObserver,
-		public  DG::CompoundItemObserver,
-		public  TBUI::IAPIToolUIDataObserver,
-		public	DG::ButtonItemObserver,
-		public	DG::BarControlObserver
-	{
-	private:
-		PolygonReducerInfoboxPage* m_tabPage;
+  class PolygonReducerPageObserver :
+    public	DG::TextEditBaseObserver,
+    public	DG::PosIntEditObserver,
+    public  DG::CompoundItemObserver,
+    public  TBUI::IAPIToolUIDataObserver,
+    public	DG::ButtonItemObserver,
+    public	DG::BarControlObserver
+  {
+  private:
+    PolygonReducerInfoboxPage* m_tabPage;
 
-		//void			GetUserDataFromDefault(WallUserData* data);
-		//void			SetUserDataToDefault(WallUserData* sdata);
-	protected:
-		virtual	void		APIElementChanged(const TBUI::APIElemDefaultFieldMask& fieldMask) override;
-		virtual void		PosIntEditChanged(const DG::PosIntEditChangeEvent& ev);
-		virtual void		ButtonClicked(const DG::ButtonClickEvent& ev) override;
-		virtual void		BarControlChanged(const DG::BarControlChangeEvent& ev) override;
+    //void			GetUserDataFromDefault(WallUserData* data);
+    //void			SetUserDataToDefault(WallUserData* sdata);
+  protected:
+    virtual	void		APIElementChanged(const TBUI::APIElemDefaultFieldMask& fieldMask) override;
+    virtual void		PosIntEditChanged(const DG::PosIntEditChangeEvent& ev);
+    virtual void		ButtonClicked(const DG::ButtonClickEvent& ev) override;
+    virtual void		BarControlChanged(const DG::BarControlChangeEvent& ev) override;
 
-	public:
-		explicit PolygonReducerPageObserver(PolygonReducerInfoboxPage* testPage);
-		~PolygonReducerPageObserver(void);
-	};
+  public:
+    explicit PolygonReducerPageObserver(PolygonReducerInfoboxPage* testPage);
+    ~PolygonReducerPageObserver(void);
+  };
 
-	// --- Wrapper class --------------------------------------------------------
+  // --- Wrapper class --------------------------------------------------------
 
-	class PolygonReducerPanel : public TBUI::APIToolUIPanel
-	{
-	private:
-		PolygonReducerPageObserver* observer;
+  class PolygonReducerPanel : public TBUI::APIToolUIPanel
+  {
+  private:
+    PolygonReducerPageObserver* observer;
 
-	public:
-		PolygonReducerPanel(Int32 refCon);
-		virtual		~PolygonReducerPanel();
+  public:
+    PolygonReducerPanel(Int32 refCon);
+    virtual		~PolygonReducerPanel();
 
-		virtual		bool	CreatePage(const DG::TabControl& tabControl, TBUI::IAPIToolUIData* data, DG::TabPage** tabPage) override;
-		virtual		void	DestroyPage(void) override;
-	};
+    virtual		bool	CreatePage(const DG::TabControl& tabControl, TBUI::IAPIToolUIData* data, DG::TabPage** tabPage) override;
+    virtual		void	DestroyPage(void) override;
+  };
 }
 #endif // !INFOBOX_PANEL_HPP
